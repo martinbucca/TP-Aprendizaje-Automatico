4b7ceb8406ddcad01f609c47589d41c07369dc7f
martinbucca <mbucca@fi.uba.ar>
2024-11-03 23:12:20 -0300

    logic

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
-from vector_rag import find_chunk
+from app import App
 from flask_cors import CORS
+from create_knowledge_graph import KnowledgeGraphBuilder
 
+chatbot = App()
 
 app = Flask(__name__)
 CORS(app)  # Esto habilita CORS para todas las rutas
@@ -11,13 +13,28 @@ CORS(app)  # Esto habilita CORS para todas las rutas
 # Endpoint para recibir queries del frontend
 @app.route('/find_chunk', methods=['POST'])
 def find_chunk_endpoint():
-    data = request.get_json()
-    query = data.get("query")
-    if query:
-        answer = find_chunk(query)
-        return jsonify({"answer": answer})
-    else:
-        return jsonify({"answer": "No se ingresó una consulta válida."}), 400
+    print("SOLICITUD DE CHUNK")
+    try:
+        data = request.get_json()
+        query = data.get("query")
+        if query:
+            answer = chatbot.answer_query(query)
+            return jsonify({"answer": answer})
+        else:
+            return jsonify({"answer": "No se ingresó una consulta válida."}), 400
+    except Exception as e:
+        return jsonify({"answer": f"No se pudo responder la consulta. Error {e}"}), 400
+    
+    
+# Endpoint para Crear grafo
+@app.route('/create_kg', methods=['POST'])
+def create_kg_endpoint():
+    try: 
+        chatbot.create_knowledge_graph()
+        return jsonify({"response": "Grafo creado exitosamente"}), 200
+    except Exception as e:
+        return jsonify({"response": "No se pudo crear el grafo a partir del documento. Error: {e}"}), 400
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/vector_rag.py
+++ backend/vector_rag.py
@@ -12,85 +12,88 @@ from transformers import AutoTokenizer, AutoModel
 from langchain_groq import ChatGroq
 
 
-llm = ChatGroq(temperature=0,model_name="mixtral-8x7b-32768")
 
-graph = Neo4jGraph(
-    url=os.getenv('NEO4J_URI'),
-    username=os.getenv('NEO4J_USERNAME'),
-    password=os.getenv('NEO4J_PASSWORD')
-)
+NUMBER_OF_DOCUMENTS_RETRIEVED = 3
+EMBEDDINGS_MODEL = "sentence-transformers/msmarco-distilbert-base-tas-b"
+MODEL_NAME="mixtral-8x7b-32768"
 
-# Create the Embedding model
-model_name = "sentence-transformers/msmarco-distilbert-base-tas-b"
-embedding_provider = HuggingFaceEmbeddings(model_name=model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModel.from_pretrained(model_name)
-if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token  
 
-# Vector store for the chunks. This will be used to retrieve the most relevant chunk for a given question.
-chunk_vector = Neo4jVector.from_existing_index(
-    embedding_provider,
-    graph=graph,
-    # The name of the index created in the previous step
-    index_name="chunkVector",
-    # The name of the property in the node that contains the embeddings
-    embedding_node_property="embedding",
-    # The name of the property in the node that contains the text
-    text_node_property="text",
-    retrieval_query="""
-// get the document
-MATCH (node)-[:PART_OF]->(d:Document)
-WITH node, score, d
-// get the entities
-MATCH (node)-[:HAS_ENTITY]-(e)
-WITH node, score, d, collect(e) as nodeslist
-// find the relationships between the entities related to the document
-MATCH p = (e)-[r]-(e2)
-WHERE e in nodeslist and e2 in nodeslist
-// unwind the path, create a string of the entities and relationships
-UNWIND relationships(p) as rels
-WITH 
-    node, 
-    score, 
-    d, 
-    collect(apoc.text.join(
-        [labels(startNode(rels))[0], startNode(rels).id, type(rels), labels(endNode(rels))[0], endNode(rels).id]
-        ," ")) as kg
-RETURN
-    node.text as text, score,
-    { 
-        document: d.id,
-        entities: kg
-    } AS metadata
-"""
-)
+class VectorRetriever:
+    def __init__(self):
+        self.graph = Neo4jGraph(
+            url=os.getenv('NEO4J_URI'),
+            username=os.getenv('NEO4J_USERNAME'),
+            password=os.getenv('NEO4J_PASSWORD')
+        )
+        self.llm = ChatGroq(
+            temperature=0,
+            model_name=MODEL_NAME,
+        )
+        self.embedding_provider = HuggingFaceEmbeddings(model_name=EMBEDDINGS_MODEL)
+        chunk_retriever = self.get_vector_index().as_retriever(search_kwargs={'k': NUMBER_OF_DOCUMENTS_RETRIEVED})
+        chunk_chain = create_stuff_documents_chain(self.llm, self.prompt())
+        self.chunk_retriever = create_retrieval_chain(
+            chunk_retriever, 
+            chunk_chain
+        )
 
-instructions = (
-    "Use the given context to answer the question."
-    "Give FIUBA students the most accurate infromation about the double degree program as if you were a student advisor."
-    "Answer what the student is asking, and give all the infromation possible from the text to support your answer."
-    "If you don't know the answer, say you don't know."
-    "Context: {context}"
-)
+    def get_vector_index(self):
+        # Vector store for the chunks. This will be used to retrieve the most relevant chunk for a given question.
+        return Neo4jVector.from_existing_index(
+            self.embedding_provider,
+            graph=self.graph,
+            # The name of the index created in the previous step
+            index_name="chunkVector",
+            # The name of the property in the node that contains the embeddings
+            embedding_node_property="embedding",
+            # The name of the property in the node that contains the text
+            text_node_property="text",
+            retrieval_query="""
+        // get the document
+        MATCH (node)-[:PART_OF]->(d:Document)
+        WITH node, score, d
+        // get the entities
+        MATCH (node)-[:HAS_ENTITY]-(e)
+        WITH node, score, d, collect(e) as nodeslist
+        // find the relationships between the entities related to the document
+        MATCH p = (e)-[r]-(e2)
+        WHERE e in nodeslist and e2 in nodeslist
+        // unwind the path, create a string of the entities and relationships
+        UNWIND relationships(p) as rels
+        WITH 
+            node, 
+            score, 
+            d, 
+            collect(apoc.text.join(
+                [labels(startNode(rels))[0], startNode(rels).id, type(rels), labels(endNode(rels))[0], endNode(rels).id]
+                ," ")) as kg
+        RETURN
+            node.text as text, score,
+            { 
+                document: d.id,
+                entities: kg
+            } AS metadata
+        """
+        )
+    def prompt(self):
+        instructions = (
+        "Brinda ayuda a los estudiantes de Ingenieria Informatica de la Universidad de Buenos Aires (Facultad de Ingenieria) respondiendo sobre las materias de la carrera."
+        "Responde conscisamente y con contexto a la pregunta."
+        "Utiliza el contexto para obtener informacion relevante a la pregunta."
+        "Si no sabes la respuesta o te falta contexto, deci que no sabes."
+        "Contexto: {context}"
+        )
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", instructions),
-        ("human", "{input}"),
-    ]
-)
+        return ChatPromptTemplate.from_messages(
+            [
+                ("system", instructions),
+                ("human", "{input}"),
+            ]
+        )
 
-chunk_retriever = chunk_vector.as_retriever()
-chunk_chain = create_stuff_documents_chain(llm, prompt)
-chunk_retriever = create_retrieval_chain(
-    chunk_retriever, 
-    chunk_chain
-)
-
-def find_chunk(q):
-    print("Finding similarities for query: ", q)
-    return chunk_retriever.invoke({"input": q})["answer"]
+    def answer_with_rag(self, q):
+        print("Finding similarities for query: ", q)
+        return self.chunk_retriever.invoke({"input": q})["answer"]
 
 
 
